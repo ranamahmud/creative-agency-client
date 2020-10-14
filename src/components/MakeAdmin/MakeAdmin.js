3db1bacfcfd73c78555e99208e3e0e452cafdce0
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-14 23:07:37 +0600

    make admin ui basic done

--- src/components/MakeAdmin/MakeAdmin.js
+++ src/components/MakeAdmin/MakeAdmin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 import Sidebar from '../Dashboard/Sidebar/Sidebar';
 import NavbarMain from '../Home/NavbarMain/NavbarMain';
 
@@ -10,16 +11,25 @@ const MakeAdmin = () => {
                 <div className="col-md-2 col-sm-6 col-12">
                     <Sidebar></Sidebar>
                 </div>
-                <div className="col-md-5 col-sm-12 col-12 d-flex justify-content-center">
-                    {/* <Calendar
-                        onChange={handleDateChange}
-                        value={new Date()}
-                    /> */}
-                    <h1>Make Admin</h1>
+                <div className="col-md-10 col-sm-12 col-12 d-flex">
+                    <Form as={Row}>
+                        <Col sm="10">
+
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                        </Col>
+                        <Col sm="2">
+                            <Button variant="primary" type="submit">
+                                Submit
+  </Button>
+                        </Col>
+
+                    </Form>
+
                 </div>
-                <div className="col-md-5 col-sm-12 col-12">
-                    {/* <AppointmentsByDate appointments={appointments}></AppointmentsByDate> */}
-                </div>
+
             </div>
         </section>
     );
