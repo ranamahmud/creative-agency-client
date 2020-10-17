5b8b588525691b1c3ebb21dcb5ab2c14f6d77966
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-17 10:33:10 +0600

    work slider done

--- src/components/Home/Works/Works.js
+++ src/components/Home/Works/Works.js
@@ -1,65 +1,70 @@
 import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import UAParser from "ua-parser-js";
+
 import './Works.css'
 const Works = () => {
-    const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
     };
+    const images = [
+        require('../../../images/carousel-1.png'),
+        require('../../../images/carousel-2.png'),
+        require('../../../images/carousel-3.png'),
+        require('../../../images/carousel-4.png'),
+        require('../../../images/carousel-5.png'),
+    ];
     return (
         <div id="works">
-            <div className="text-center">
+            <div className="text-center" id="works-text">
                 <h2>Here are some of <span style={{ color: '#7AB259' }}>our works</span></h2>
             </div>
 
+            <div id="works-carousel">
+                <Carousel
+                    ssr
+                    // deviceType={deviceType}
+                    itemClass="image-item"
+                    responsive={responsive}
+                    infinite={true}
+                    showDots={true}
+                    autoPlay={true}
+                    autoPlaySpeed={2000}
+                >
+                    {images.map(image => {
+                        return (
+                            <Image fluid
+                                draggable={false}
+                                style={{
+                                    width: "468.1px",
+                                    height: "336.53px",
 
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={require('../../../images/carousel-1.png')}
-                        alt="First slide"
-                    />
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={require('../../../images/carousel-2.png')}
-                        alt="Second slide"
-                    />
-
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={require('../../../images/carousel-3.png')}
-                        alt="Third slide"
-                    />
-
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={require('../../../images/carousel-4.png')}
-                        alt="Third slide"
-                    />
-
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={require('../../../images/carousel-5.png')}
-                        alt="Third slide"
-                    />
-
-
-                </Carousel.Item>
-            </Carousel>
+                                }}
+                                src={image}
+                            />
+                        );
+                    })}
+                </Carousel>
+            </div>
         </div>
     );
 };
