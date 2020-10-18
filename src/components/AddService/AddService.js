77d1a4b1877231e73bac95811b4c2c29c262f53b
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-18 08:44:36 +0600

    add service form ui updated

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -1,18 +1,71 @@
-import React from 'react';
-import { Button, Form, Navbar } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Form, Image, Navbar } from 'react-bootstrap';
 import Sidebar from '../Dashboard/Sidebar/Sidebar';
 import NavbarMain from '../Home/NavbarMain/NavbarMain';
+import './AddService.css'
+import { useAlert } from 'react-alert'
 
 const AddService = () => {
+    const alert = useAlert()
+    const [info, setInfo] = useState({
+        status: 'Pending'
+    });
+    const [file, setFile] = useState(null);
+    const handleBlur = e => {
+        const newInfo = { ...info };
+        newInfo[e.target.name] = e.target.value;
+        setInfo(newInfo);
+    }
+
+    const handleFileChange = (e) => {
+        const newFile = e.target.files[0];
+        console.log(newFile)
+        setFile(newFile);
+    }
+
+    const handleSubmit = (e) => {
+        const formData = new FormData()
+        console.log(info);
+        if (file === null) {
+            alert.error('You must select a image file')
+
+        } else {
+            formData.append('file', file);
+            formData.append('userName', info.userName);
+            formData.append('email', info.email);
+            formData.append('name', info.name);
+            formData.append('details', info.details);
+            formData.append('price', info.price);
+            console.log(info);
+            // fetch('https://salty-plateau-71286.herokuapp.com/addADoctor', {  
+            fetch('http://localhost:5000/addOrder', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.json())
+                .then(data => {
+                    alert.success('Order Placed Successfully!')
+                    document.querySelector('#order-form').reset()
+                })
+                .catch(error => {
+                    alert.error('Order Placed Failed!')
+
+                    console.error(error)
+                })
+
+        }
+
+        e.preventDefault()
+    }
     return (
         <section>
             <NavbarMain />
             <div className="container-fluid row">
                 <Sidebar></Sidebar>
                 <div className="col-md-5 col-sm-12 col-12 d-flex">
-                    <Form>
+                    <Form id="add-service">
                         <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Service Title</Form.Label>
+                            <Form.Label >Service Title</Form.Label>
                             <Form.Control type="email" placeholder="Enter title" />
                         </Form.Group>
 
@@ -26,19 +79,29 @@ const AddService = () => {
                     </Form>
                 </div>
                 <div className="col-md-5 col-sm-12 col-12">
-                    <Form.Group>
-                        {/* <input onChange={handleFileChange} type="file" className="form-control" id="fileUpload" /> */}
+                    <div className="form-group"
+                        id="upload-img-form"
+                    >
+                        <div className="custom-file"
+                        >
+                            <label id="upload-btn-img" className="custom-file-label" htmlFor="customFile"
+                            ><Image src={require('../../images/icons/upload-image.png')}
+                                style={{ width: "24px", height: "24px" }}
+                                />Upload image</label>
+                            <input type="file"
+                                onChange={handleFileChange}
+                                id="customFile"
+                                hidden
+                            />
 
-                        <Form.Label for="files" class="btn">Upload Image</Form.Label>
-                        <input id="files"
-                            style={{ visibility: 'hidden' }}
-                            type="file" />
+                        </div>
+                    </div>
 
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <button id="add-btn"
+                        className="text-white"
+                    >
                         Submit
-  </Button>
-
+  </button>
                 </div>
 
             </div>
