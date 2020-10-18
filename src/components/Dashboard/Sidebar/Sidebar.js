d82e3868da5ba40c6db2df697462188526cc379b
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-19 04:31:39 +0600

    minor changed

--- src/components/Dashboard/Sidebar/Sidebar.js
+++ src/components/Dashboard/Sidebar/Sidebar.js
@@ -3,8 +3,7 @@ import { Image, Nav, Navbar } from 'react-bootstrap';
 import { Link, NavLink, Redirect, useHistory, useLocation } from 'react-router-dom';
 import { UserContext } from '../../../App';
 import './Sidebar.css'
-const Sidebar = (props) => {
-    const location = useLocation();
+const Sidebar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const [isAdmin, setIsAdmin] = useState(false);
 
@@ -25,72 +24,52 @@ const Sidebar = (props) => {
         <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{ height: "50vh" }}>
             <ul id="sidebar-list" className="list-unstyled">
 
-                {
-                    isAdmin === false &&
-                    <div>
-                        <li>
-                            <Link to="/order" >
-                                <Image src={require('../../../images/icons/shopping-cart.png')} /><span>Order</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/service-list" >
-                                <Image src={require('../../../images/icons/service-list.png')} /><span>Service List</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/review" >
-                                <Image src={require('../../../images/icons/review.png')} /> <span>Review</span>
-                            </Link>
-                        </li>
 
-                    </div>
-                }
-                {/* {
-                    (isAdmin === true
-                        // &&
-                        // (
-                        //     location.pathname !== '/review' ||
-                        //     location.pathname !== '/service-list-admin'
 
-                        // )
-
-                    ) &&
-                    (
-                        <Redirect
-                            to={{
-                                pathname: "/service-list-admin",
-                                state: { from: props.location }
-                            }}
-                        />
-                    )
-                } */}
 
                 {
 
-                    isAdmin === true &&
-                    <div>
-                        <li>
-                            <Link to="/service-list-admin" >
-                                <Image src={require('../../../images/icons/service-list.png')} />
-                                <span>Service List</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/add-service" >
-                                <Image src={require('../../../images/icons/plus.png')} />
-                                <span>Add Service</span>
+                    isAdmin === true ?
+                        <div>
+                            <li>
+                                <Link to="/service-list-admin" >
+                                    <Image src={require('../../../images/icons/service-list.png')} />
+                                    <span>Service List</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/add-service" >
+                                    <Image src={require('../../../images/icons/plus.png')} />
+                                    <span>Add Service</span>
 
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/make-admin" >
-                                <Image src={require('../../../images/icons/make-admin.png')} />
-                                <span>Make Admin</span>
-                            </Link>
-                        </li>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/make-admin" >
+                                    <Image src={require('../../../images/icons/make-admin.png')} />
+                                    <span>Make Admin</span>
+                                </Link>
+                            </li>
 
-                    </div>
+                        </div> :
+                        <div>
+                            <li>
+                                <Link to="/order" >
+                                    <Image src={require('../../../images/icons/shopping-cart.png')} /><span>Order</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/service-list" >
+                                    <Image src={require('../../../images/icons/service-list.png')} /><span>Service List</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/review" >
+                                    <Image src={require('../../../images/icons/review.png')} /> <span>Review</span>
+                                </Link>
+                            </li>
+
+                        </div>
                 }
             </ul>
         </div>
